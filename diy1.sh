4035554a15a041a4c813e16d49cfca9f20b748ea
coolook <coolook@users.noreply.github.com>
2024-07-30 11:48:20 +0800

    Update diy1.sh

--- diy1.sh
+++ diy1.sh
@@ -3,3 +3,13 @@
 # Please enter the custom command below (usually used to install third-party plugins) (can be left blank)
 # git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
 # git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
+
+# 大鹅
+git clone https://github.com/QiuSimons/luci-app-daed package/dae
+
+# drop mosdns and v2ray-geodata packages that come with the source
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
