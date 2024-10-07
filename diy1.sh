7a8810b58e6f13ae1d2f67236842865d8d67a46b
coolook <coolook@users.noreply.github.com>
2024-10-07 22:13:49 +0800

    Update diy1.sh

--- diy1.sh
+++ diy1.sh
@@ -13,3 +13,6 @@ find ./ | grep Makefile | grep mosdns | xargs rm -f
 
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone https://github.com/morytyann/OpenWrt-mihomo package/mihomo
+
+sed -i 's/192.168.1.1/10.0.0.252/g' package/base-files/files/bin/config_generate
